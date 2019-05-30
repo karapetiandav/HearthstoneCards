6b017ba461066aaa993c606dfb53c40661aaea34
Davit Karapetian <dkarapetyan@more.tv>
2019-05-30 18:20:16 +0300

    Initial commit
    Create project

---
+++ app/src/main/java/ru/karapetiandav/hearthstonecards/network/ApiService.kt
@@ -0,0 +1,35 @@
+package ru.karapetiandav.hearthstonecards.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiService {
+
+    private val BASE_URL = "https://omgvamp-hearthstone-v1.p.rapidapi.com/"
+    private val retrofit: Retrofit
+
+    init {
+        val httpClient = buildHttpClient()
+
+        retrofit = Retrofit.Builder()
+            .client(httpClient)
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun buildHttpClient(): OkHttpClient {
+
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+}
