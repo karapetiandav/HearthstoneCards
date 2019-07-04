e5c351ce9f8a41df90c75e15bd365ff023124238
Davit Karapetian <karapetiandav@gmail.com>
2019-07-04 16:31:50 +0300

    Replace DetailedCard with Card model

--- app/src/main/java/ru/karapetiandav/hearthstonecards/features/cards/models/Card.kt
+++ app/src/main/java/ru/karapetiandav/hearthstonecards/features/cards/models/Card.kt
@@ -1,12 +1,23 @@
 package ru.karapetiandav.hearthstonecards.features.cards.models
 
 class Card(
-    val cardId: String,
-    val dbfId: String,
-    val name: String,
-    val cardSet: String,
-    val type: String,
-    val text: String,
-    val playerClass: String,
-    val locale: String
+    val cardId: String?,
+    val name: String?,
+    val cardSet: String?,
+    val type: String?,
+    val faction: String?,
+    val rarity: String?,
+    val cost: Int?,
+    val attack: Int?,
+    val health: Int?,
+    val text: String?,
+    val flavor: String?,
+    val artist: String?,
+    val collectible: Boolean?,
+    val elite: Boolean?,
+    val race: String?,
+    val playerClass: String?,
+    val img: String?,
+    val imgGold: String?,
+    val locale: String?
 )
